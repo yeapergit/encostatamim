7d6c5da5d8e2e5d075b28d1f0980fc6bbbc9cce2
Pedro <yeaper@gmail.com>
2020-02-04 16:56:47 +0000

    Dummy table

--- src/components/CustomTable.js
+++ src/components/CustomTable.js
@@ -5,7 +5,78 @@ class CustomTable extends React.Component {
   render() {
     return (
       <Table compact fixed>
-       
+        <Table.Header fullWidth>
+          <Table.Row>
+            <Table.HeaderCell>Nome</Table.HeaderCell>
+            <Table.HeaderCell>Nif</Table.HeaderCell>
+            <Table.HeaderCell>Número Sócio</Table.HeaderCell>
+            <Table.HeaderCell/>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>John Lilki</Table.Cell>
+            <Table.Cell>123456789</Table.Cell>
+            <Table.Cell>19823798237</Table.Cell>
+            <Table.Cell>
+              <Button primary>Ver</Button>
+              <Button secondary>Editar</Button>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>John Lilki</Table.Cell>
+            <Table.Cell>123456789</Table.Cell>
+            <Table.Cell>19823798237</Table.Cell>
+            <Table.Cell>
+              <Button primary>Ver</Button>
+              <Button secondary>Editar</Button>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>John Lilki</Table.Cell>
+            <Table.Cell>123456789</Table.Cell>
+            <Table.Cell>19823798237</Table.Cell>
+            <Table.Cell>
+              <Button primary>Ver</Button>
+              <Button secondary>Editar</Button>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+
+                <Table.Body>
+          <Table.Row>
+            <Table.Cell>John Lilki</Table.Cell>
+            <Table.Cell>123456789</Table.Cell>
+            <Table.Cell>19823798237</Table.Cell>
+            <Table.Cell>
+              <Button primary>Ver</Button>
+              <Button secondary>Editar</Button>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+
+        <Table.Footer fullWidth>
+          <Table.Row>
+            <Table.HeaderCell colSpan="4">
+              <Button
+                floated="right"
+                icon
+                labelPosition="left"
+                primary
+                size="small"
+              >
+                <Icon name="user" /> Adicionar Pessoa
+              </Button>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
       </Table>
     );
   }
